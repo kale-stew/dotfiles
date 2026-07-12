a42d2342ec5ad56e8e52f2dd8b31fce4e6e2d836
Kylie Czajkowski <hello@kylieis.online>
2026-07-11 20:30:45 -0700

    prioritize personal opencode launchers

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,4 +1,4 @@
 typeset -U path
-path=(/opt/homebrew/bin /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin "$ZSH/bin" $path)
+path=("$HOME/.local/bin" /opt/homebrew/bin /usr/local/bin /usr/local/sbin /usr/bin /bin /usr/sbin /sbin "$ZSH/bin" $path)
 export PATH
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
